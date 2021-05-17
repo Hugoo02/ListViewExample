cdd10190314f0c687cb77dacebf73b6c020628f0
hugodaniel170902@agmail.com <HugoOliveira02>
2021-05-17 16:31:03 +0100

    almost done

--- app/src/main/java/com/example/listviewexample/EditGame.kt
+++ app/src/main/java/com/example/listviewexample/EditGame.kt
@@ -1,24 +1,27 @@
 package com.example.listviewexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class EditGame : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_game)
 
-        //var editTextTitle = findViewById<EditText>(R.id.editTextTitle)
-        //var editTextGender = findViewById<EditText>(R.id.editTextGender)
-        //val buttonEdit = findViewById<Button>(R.id.buttonEdit)
+        var editTextTitle = findViewById<EditText>(R.id.editTextTitle)
+        var editTextGender = findViewById<EditText>(R.id.editTextGender)
+        val buttonEdit = findViewById<Button>(R.id.buttonEdit)
 
         val bundle = intent.extras
 
         bundle?.let {
-            editTextTitle.text = it.getString("title")
+            editTextTitle.hint = it.getString("title")
+            editTextGender.hint = it.getString("gender")
         }
+
 
     }
 }
